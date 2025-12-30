542b6301a812faea6054c301a667e0a2f4acfb86
Furkan EREN <furkaneren.bme@gmail.com>
2025-12-30 22:31:56 +0300

    Cmake ve main cpp update for raspberrpi env.

--- NightStand/main.cpp
+++ NightStand/main.cpp
@@ -3,16 +3,16 @@
 
 int main(int argc, char *argv[])
 {
+    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("NightStand", "Main");
-
+    const QUrl url(u"qrc:/NightStand/Main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+                         if (!obj && url == objUrl)
+                             QCoreApplication::exit(-1);
+                     }, Qt::QueuedConnection);
+    engine.load(url);
     return app.exec();
 }
